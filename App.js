e8f89d0552558d21771b05467c268bc310a04f1c
uni-fied <devadiroot@gmail.com>
2022-12-28 07:57:42 +0700

    Belajar membuat view dengan atribut style serta mengatur margin Layout utama

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const LotOfStyle = () => {
   return (
     <View style={styles.container}>
-      <Text>Aplikasi React Native Pertama Saya</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.orange}>just orange</Text>
+        <Text style={styles.bigBlue}>just bigBlue</Text>
+        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10,
   },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  orange: {
+    color: 'orange',
+  }
 });
+
+export default LotOfStyle;
